62ca01fff75628c9815af40e6f10f55e9bd3f27e
tomlilly <tomlilly@protonmail.com>
2023-05-09 12:06:24 +1000

    first commit

---
+++ scanNow/scanNowFix.bat
@@ -0,0 +1,30 @@
+@echo off
+
+rem create script file
+echo d = date () - 1 > yesterday.vbs
+rem ddmmyyyy
+echo wscript.echo right (100 + day (d),2) & right (100 + month (d),2) & year (d) >> yesterday.vbs
+rem yyyymmdd
+:: echo wscript.echo year (d) * 10000 + month (d) * 100 + day (d) >> yesterday.vbs
+
+
+rem store yesterdays date in variable yesterday
+for /f %%a in ('cscript //nologo yesterday.vbs') do set yesterday=%%a
+
+rem create the reg file
+echo Windows Registry Editor Version 5.00 > update.reg
+rem insert key between [], appends to update.reg
+echo [] >> update.reg
+rem insert value to be updated between ""
+echo ""="%yesterday%" >> update.reg
+rem insert key between [], appends to update.reg
+echo [] >> update.reg
+rem insert value to be updated between ""
+echo ""="%yesterday%" >> update.reg
+
+rem update the registry
+reg import update.reg
+
+rem delete temp files
+del yesterday.vbs
+del update.reg
