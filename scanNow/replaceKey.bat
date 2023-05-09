05159b6b87f27a555be853f31b3d55eff4edf5f8
tomlilly <tomlilly@protonmail.com>
2023-05-09 14:11:52 +1000

    replaceKey.reg update

--- scanNow/replaceKey.bat
+++ scanNow/replaceKey.bat
@@ -1,5 +1,9 @@
 @echo off
-REM Copy the old key to the new key
+
+REM Copy old key value to the new key with updated name eg "\lastProcessed_BAK"
 REG COPY [OldKey] [NewKey] /s /f
+
 REM Delete the old key
 REG DELETE [OldKey] /f
+
+REM results in existing key value stored in newly named key as backup, default key will be created at run
