4021fce1a8442fe97ca9278ec748aa07e9db45aa
tomlilly <102399843+tomlilly@users.noreply.github.com>
2023-05-10 16:43:57 +1000

    Added specific keys and values

--- scanNow/replaceKey.bat
+++ scanNow/replaceKey.bat
@@ -1,9 +1,13 @@
 @echo off
 
 REM Copy old key value to the new key with updated name eg "\lastProcessed_BAK"
-REG COPY [OldKey] [NewKey] /s /f
+REM REG COPY [OldKey] [NewKey] /s /f
+REG COPY [HKEY_CURRENT_USER\Software\VB and VBA Program Settings\Locus\DMEmail\InBoxLastProcessedDate] [HKEY_CURRENT_USER\Software\VB and VBA Program Settings\Locus\DMEmail\InBoxLastProcessedDate_BAK] /s /f
+REG COPY [HKEY_CURRENT_USER\Software\VB and VBA Program Settings\Locus\DMEmail\SentItemLastProcessedDate] [HKEY_CURRENT_USER\Software\VB and VBA Program Settings\Locus\DMEmail\SentItemLastProcessedDate_BAK] /s /f
 
 REM Delete the old key
-REG DELETE [OldKey] /f
+REM REG DELETE [OldKey] /f
+REG DELETE [HKEY_CURRENT_USER\Software\VB and VBA Program Settings\Locus\DMEmail\InBoxLastProcessedDate] /f
+REG DELETE [HKEY_CURRENT_USER\Software\VB and VBA Program Settings\Locus\DMEmail\SentItemLastProcessedDate] /f
 
 REM results in existing key value stored in newly named key as backup, default key will be created at run
