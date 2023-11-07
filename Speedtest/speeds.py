44e6f9ae5d446acde11e73b2242234819502d323
tomlilly <tomlilly@protonmail.com>
2023-11-08 09:47:14 +1100

    updated speeds.py

--- Speedtest/speeds.py
+++ Speedtest/speeds.py
@@ -2,12 +2,7 @@ import pandas as pd
 import re
 
 # Initialize lists to store data
-dates = []
-times = []
-servers = []
-isps = []
-downloads = []
-uploads = []
+dates, times, servers, isps, downloads, uploads = [], [], [], [], [], []
 
 # Open and read the file
 with open('speedtest.txt', 'r') as file:
@@ -16,24 +11,20 @@ with open('speedtest.txt', 'r') as file:
 # Loop over the lines and extract the data
 for line in lines:
     if re.search(r'\d{2}/\d{2}/\d{4}', line):
-        date = re.search(r'\d{2}/\d{2}/\d{4}', line).group()
-        time = re.search(r'\d{2}:\d{2}:\d{2}', line).group()
-        if date_search is not None and time_search is not None:
-            date = date_search.group()
-            time = time_search.group()
-            dates.append(date)
-            times.append(time)
+        _, date, time = line.strip().split(maxsplit=2)
+        dates.append(date)
+        times.append(time)
     elif 'Server:' in line:
-        server = line.split('Server:')[1].strip()
+        server = line.split(': ')[1].strip()
         servers.append(server)
     elif 'ISP:' in line:
-        isp = line.split('ISP:')[1].strip()
+        isp = line.split(': ')[1].strip()
         isps.append(isp)
     elif 'Download:' in line:
-        download = float(re.search(r'\d+\.\d+', line).group())
+        download = line.split()[1]
         downloads.append(download)
     elif 'Upload:' in line:
-        upload = float(re.search(r'\d+\.\d+', line).group())
+        upload = line.split()[1]
         uploads.append(upload)
 
 # Create a DataFrame from the lists
@@ -42,9 +33,9 @@ df = pd.DataFrame({
     'Time': times,
     'Server': servers,
     'ISP': isps,
-    'Download': downloads,
-    'Upload': uploads
+    'Download': pd.to_numeric(downloads),
+    'Upload': pd.to_numeric(uploads)
 })
 
 # Write the DataFrame to an Excel file
-df.to_excel('speedtest_data.xlsx', index=False)
+df.to_excel('speedtest.xlsx', index=False)
